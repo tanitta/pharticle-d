ee5193102854af62355f5cda6a4a27f530337509
tanitta <ttata.trit@gmail.com>
2016-06-21 22:00:29 +0900

    Remove neadless attribute.

--- source/pharticle/constraintpair.d
+++ source/pharticle/constraintpair.d
@@ -4,7 +4,7 @@ struct ConstraintPair {
 		pharticle.Particle[] particles(){return _particlePtrs;}
 		void delegate(ref pharticle.Particle, ref pharticle.Particle) forceFunction(){return _forceFunction;}
 		
-		this(ref pharticle.Particle p1, ref pharticle.Particle p2, void delegate(ref pharticle.Particle, ref pharticle.Particle) func){
+		this(pharticle.Particle p1, pharticle.Particle p2, void delegate(ref pharticle.Particle, ref pharticle.Particle) func){
 			_particlePtrs[0] = p1;
 			_particlePtrs[1] = p2;
 			_forceFunction = func;
